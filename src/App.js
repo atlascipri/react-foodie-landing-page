f514516d388e2584544770c707ecd3792fcf76b6
atlascipriano <atlasciprianoo@gmail.com>
2024-10-28 10:09:36 -0300

    feat: cria o componente Work

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Home from './Components/Home.js'
 import About from './Components/About.js'
+import Work from './Components/Work.js'
 
 function App() {
   return (
     <div className="App"> 
       <Home> </Home>
       <About> </About>
+      <Work> </Work>
     </div>
   );
 }
