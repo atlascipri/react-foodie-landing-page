3c17dd9970a417cf9424ee88f5a33d84200bb982
atlascipriano <atlasciprianoo@gmail.com>
2024-10-28 10:33:48 -0300

    feat: cria o componente Testimonials

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './Components/Home.js'
 import About from './Components/About.js'
 import Work from './Components/Work.js'
+import Testimonials from './Components/Testimonials.js';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Home> </Home>
       <About> </About>
       <Work> </Work>
+      <Testimonials> </Testimonials>
     </div>
   );
 }
---
+++ src/Components/Testimonials.js
@@ -0,0 +1,37 @@
+import React from "react";
+import ProfilePicture from "../Assets/john-doe-image.png";
+import { AiFillStar } from "react-icons/ai";
+
+const Testimonials = () => {
+  return (
+    <div className="work-section-wrapper">
+      <div className="work-section-top"> 
+        <p className="primary-subheading"> Testimonials </p>
+        <h2 className="primary-heading"> What They Are Saying </h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et
+          elit. Dolor turpis molestie dui magnis facilisis at fringilla quam.
+        </p>
+      </div>
+      <div className="testimonial-section-bottom">
+        <div>
+          <img src={ProfilePicture} alt="" />
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et
+          elit. Dolor turpis molestie dui magnis facilisis at fringilla quam.
+        </p>
+        <div className="testimonials-stars-container">
+          <AiFillStar />
+          <AiFillStar />
+          <AiFillStar />
+          <AiFillStar />
+          <AiFillStar />
+        </div>
+        <h2> John Doe </h2>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonials;
