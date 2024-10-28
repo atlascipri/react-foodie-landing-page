142d3a26d127c91d1bb4352074aee72819e35e6f
atlascipriano <atlasciprianoo@gmail.com>
2024-10-28 10:56:10 -0300

    feat: cria o componente Contact

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import Home from './Components/Home.js'
 import About from './Components/About.js'
 import Work from './Components/Work.js'
-import Testimonials from './Components/Testimonials.js';
+import Testimonials from './Components/Testimonials.js'
+import Contact from './Components/Contact.js'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <About> </About>
       <Work> </Work>
       <Testimonials> </Testimonials>
+      <Contact> </Contact>
     </div>
   );
 }
